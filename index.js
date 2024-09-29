12c7097f5d08220ae0a2c00013a9cb12ef0d0af0
Dev-Fel254 <odhisfello99@gmail.com>
2024-09-29 20:33:44 +0300

    Create a hover effect use of onmouseover event listener to cause a hover effect on the grid elements

--- index.js
+++ index.js
@@ -2,5 +2,8 @@ for(x=0; x<256; x++){
     const myDivs = document.createElement("div");
     myDivs.className = "grid-item";
     document.getElementById('demo').appendChild(myDivs);
+    myDivs.addEventListener("mouseover", ()=>{
+        myDivs.style.backgroundColor = "black";
+    });
 }
 
