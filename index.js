54655f0fcb3cd86440af03ef775340d78a4c996e
Dev-Fel254 <odhisfello99@gmail.com>
2024-09-29 19:23:46 +0300

    Create 16x16 grid using flexbox Create div elements using DOM and append them to the container

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-const myDivs = document.createElement("div");
+for(x=0; x<256; x++){
+    const myDivs = document.createElement("div");
+    myDivs.className = "grid-item";
+    document.getElementById('demo').appendChild(myDivs);
+}
+
