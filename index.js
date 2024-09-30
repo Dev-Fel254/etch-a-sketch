45c16ad55f70941e4251916d6c96cc52c9e765eb
Dev-Fel254 <odhisfello99@gmail.com>
2024-10-01 00:59:22 +0300

    Create a button that triggers a prompt The user enters number of squares per side for a new grid

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
-for(x=0; x<256; x++){
-    const myDivs = document.createElement("div");
-    myDivs.className = "grid-item";
-    document.getElementById('demo').appendChild(myDivs);
-    myDivs.addEventListener("mouseover", ()=>{
-        myDivs.style.backgroundColor = "black";
+const title = document.createElement("h1");
+const btn = document.createElement("button");
+
+title.textContent = "Click the button to generate your desired grid: i.e 9 for 9x9";
+btn.textContent = "Grid";
+document.body.appendChild(title);
+document.body.appendChild(btn);
+
+    //btn triggers the subsequent events
+    btn.addEventListener("click", function () {
+       const gridContainer = document.getElementById("demo");
+       document.body.appendChild(gridContainer);
+       gridContainer.innerHTML = "";
+
+        const size = parseInt(prompt("Enter grid size(e.g., 9 for 9x9 grid)"));
+        const gridSize = parseInt(size*size);
+        console.log(gridSize);
+        
+        //This ensures the grid takes input between 1 and 31
+        if(Number(gridSize) && gridSize > 0 && gridSize<= 1000){
+            for(let i=1; i<=gridSize; i++){
+                    const myDivs = document.createElement("div");
+                    myDivs.className = "grid-item";
+                    myDivs.innerText = i;
+                    document.getElementById('demo').appendChild(myDivs);
+                    myDivs.addEventListener("mouseover", ()=>{
+                        myDivs.style.backgroundColor = "black"; 
+                        });
+                }
+            }else{
+                alert("Please enter a number in this range (1-31).");
+        }
     });
-}
+    
 
